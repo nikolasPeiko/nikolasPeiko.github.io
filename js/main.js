628d8381b4af2e11523937289307a104dde1a441
nickyshibaev@gmail.com <GrAnT0132>
2019-04-05 16:32:46 +0300

    'zalupa'

--- js/main.js
+++ js/main.js
@@ -60,20 +60,20 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 4);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
-/******/ ({
-
-/***/ 4:
+/******/ ([
+/* 0 */,
+/* 1 */,
+/* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(5);
+module.exports = __webpack_require__(3);
 
 
 /***/ }),
-
-/***/ 5:
+/* 3 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -81,320 +81,111 @@ module.exports = __webpack_require__(5);
 
 console.log('Hello from main.js!');
 
-$('.insurance-products').slick({
-    infinite: true,
-    dots: false,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    arrows: true,
-    centerMode: false,
-    variableWidth: true,
-    focusOnSelect: true,
-    prevArrow: $('.insurance__top .prevSlick'),
-    nextArrow: $('.insurance__top .nextSlick')
-});
+function modalSuccess() {
 
-function opacitySlide() {
-    var slideActive = $('.insurance-products .slick-active');
-    var slide = $('.insurance-products .slick-slide');
-    $(slide).addClass('opacity');
-    $(slide[0]).removeClass('opacity');
-    $(slide[1]).removeClass('opacity');
-    $(slide[2]).removeClass('opacity');
-    $(slideActive).removeClass('opacity');
-    $(slideActive[3]).addClass('opacity');
-}
-
-opacitySlide();
-$('.insurance-products').on('swipe', function (event, slick, currentSlide, nextSlide) {
-    opacitySlide();
-});
-$('.insurance-products').on('afterChange', function (event, slick, currentSlide, nextSlide) {
-    opacitySlide();
-});
-$('.clients').slick({
-    infinite: true,
-    dots: false,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: true,
-    centerMode: false,
-    variableWidth: true,
-    focusOnSelect: true
-});
-$('.reviews__slide__wrap').slick({
-    infinite: true,
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    prevArrow: $('.reviews__top .prevSlick'),
-    nextArrow: $('.reviews__top .nextSlick')
-});
-
-function about__text() {
-    var text = $('.text__block');
-    $('.showText').on('click', function () {
-        if ($(text).hasClass('active')) {
-            $(this).parent().find('.text__block').removeClass('active');
-            $(this).text('Читать подробнее');
-        } else {
-            $(this).parent().find('.text__block').addClass('active');
-            $(this).text('Скрыть');
-        }
-    });
-}
-
-about__text();
-
-function naw__menu() {
-    $('.nav__link, .header__wrapper__about__item__lang').on('click', function () {
-        if ($(this).find('.nav__link__sub').length > 0 && !$(this).hasClass('open')) {
-            $('.nav__link').removeClass('open');
-            $(this).addClass('open');
-        } else if ($(this).hasClass('open')) {
-            $(this).removeClass('open');
-        }
-    });
-}
-
-naw__menu();
-
-$('.hero__bg__wrap').slick({
-    infinite: true,
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 4000
-});
-
-//кастомный селект
-
-var SelectList = {
-    fn: {
-        prepare: function prepare() {
-            $('.select').each(function () {
-                var select = $('<div class="select-box"/>');
-                var html = '<div class="trigger"></div>';
-                html += '<ul class="choices">';
-                var $option = $(this);
-                $option.find('option').each(function (key, val) {
-                    var value = $(val).val();
-
-                    html += '<a href="#" data-value="' + value + '">' + value + '</a>';
-                });
-                html += '</ul>';
-                select.html(html).insertBefore($($option));
-            });
-        },
-
-        showHide: function showHide() {
-            $('.trigger', '.select-box').on('click', function () {
-                var $trigger = $(this);
-                var list = $trigger.next();
-                if (list.is(':hidden')) {
-                    list.slideDown(300);
-                    list.css('display', 'flex');
-                    $(this).addClass('open');
-                } else {
-                    $(this).removeClass('open');
-                    list.slideUp(300);
-                }
-            });
-        },
-
-        view: function view() {
-            $('body').on('click', function () {
-                var value = $('.select');
-                // console.log($(value).val())
-            });
-        },
-
-        select: function select() {
-            var $trigger = $('.trigger');
-            var $select = $('.select');
-            var $view = $('.view');
-            var startText = $($trigger).siblings(".choices").find('a:first-child').text();
-            // console.log($($trigger).siblings(".choices").find('li:first-child').text())
-            $($trigger).text(startText);
-            $('a', '.choices').on('click', function () {
-                $(this).parents('.calc__form_row').find('.trigger').removeClass('open');
-                var $li = $(this);
-
-                var value = $li.data('value');
-                $(this).parents('.select-box').find('.trigger').text(value);
-
-                $li.parent().slideUp(300, function () {
-
-                    $(this).parents('.calc__form_row').find('.select').val(value);
-                    $view.trigger('click');
-                });
-            });
-        }
-    },
-
-    init: function init() {
-        for (var method in this.fn) {
-            this.fn[method]();
-        }
+    var btn = document.querySelectorAll("a.btn__modal");
+    var modal = document.body.querySelector('div.modal__wrap');
+    var close = modal.parentElement.querySelector('div.close');
+    for (var i = 0; i < btn.length; i++) {
+        btn[i].addEventListener('click', function () {
+            modal.classList.add("active");
+            close.onclick = function () {
+                modal.classList.remove("active");
+            };
+        });
     }
-};
+}
 
-$(function () {
-    SelectList.init();
-});
-
-// кастомный селект=======================end
-
-//табы для фильтра==================================
-function filterTabs() {
-    var filterSelect = $('.filter__item__select');
-    $(filterSelect).on('click', function () {
-        $(this).parent().find('.filter__item__list').toggleClass('open');
-        if ($(this).parent().find('.filter__item__list').hasClass('open')) {
-            $(this).addClass('open');
-        } else {
-            $(this).removeClass('open');
-        }
-
-        $(this).parent().find('.filter__item__list .item').on('click', function () {
-            console.log($(this).parent().parent());
-            var text = $(this).text();
-            $(this).parent().parent().find('.filter__item__select__caption').text(text);
-            $(this).parent().removeClass('open');
-            $(this).parent().parent().find('.filter__item__select').removeClass('open');
+modalSuccess();
+function recovery__success() {
+    $('.recovery__success').on('click', function () {
+        $(this).parent().toggle('active');
+        $('.modal__recovery__success').show().css('display', 'flex');
+        $('.modal__recovery__success').find('a.button').on('click', function () {
+            $('.modal__wrap').removeClass('active');
         });
     });
 }
+recovery__success();
 
-filterTabs();
-//табы для фильтра==================================end
-
-//рейтинг - звезды ======================================
-function starRang() {
-    var stars = $('.stars');
-    var rating = $('.rating').text();
-    console.log(rating);
-}
-
-starRang();
-//рейтинг - звезды ======================================end
-
-
-$('.tabs-bar__wrap__button').find('.item').on('click', function () {
-    $('.tabs-bar__wrap__button').find('.item').removeClass('active');
-    $(this).addClass('active');
-});
-
-$('.filter__item').find('.item').on('click', function () {
-    $('.filter__item').find('.item').removeClass('active');
-    $(this).addClass('active');
-});
-
-$(window).on('scroll', function () {
-    var btnTop = $('.social__fix');
-    if ($(window).scrollTop() > 500) {
-        $(".social__fix").addClass("fixed");
-    } else {
-        //remove the background property so it comes transparent again (defined in your css)
-        $(".social__fix").removeClass("fixed");
-    }
-});
-// $(window).on("scroll", function () {
-//     if (come("#btnTop")) {
-//         $(".go-to-top").removeClass("fixed");
-//     }
-// });
-$(window).on('scroll', function () {
-    if ($(window).scrollTop() > 40) {
-        $('.container-header').addClass('mini');
-    } else {
-        $('.container-header').removeClass('mini');
-    }
-});
-//аккордеон========================================================
-function acordion() {
-    $('.open_acordion').on('click', function () {
-
-        if ($(this).parent().parent().find('.block__acordion').hasClass('open')) {
-            $(this).parent().parent().find('.block__acordion').removeClass('open');
-            $(this).find('span').text('Раскрыть').addClass('active');
-            $(this).removeClass('active');
-        } else {
-            $('.block__acordion').removeClass('open');
-            $(this).parent().parent().find('.block__acordion').addClass('open');
-            $('.open_acordion').find('span').text('Раскрыть');
-            $(this).find('span').text('Cвернуть').removeClass('active');
-            $(this).addClass('active');
+function tabsRegistration() {
+    $('.log__wrap__tabs__row .button').on('click', function () {
+        $('.log__wrap__tabs__row .button').removeClass('active');
+        $(this).addClass('active');
+        if ($('.log__wrap__tabs__row .button.email').hasClass('active')) {
+            $('.form.phone').removeClass('open');
+            $('.form.email').addClass('open');
+        }
+        if ($('.log__wrap__tabs__row .button.phone').hasClass('active')) {
+            $('.form.email').removeClass('open');
+            $('.form.phone').addClass('open');
         }
     });
 }
+tabsRegistration();
 
-acordion();
-//аккордеон========================================================end
+function customSelect() {
+    $('.custom__select').styler();
+}
 
+customSelect();
 
-$(function () {
-    // Initializes and creates emoji set from sprite sheet
-    window.emojiPicker = new EmojiPicker({
-        emojiable_selector: '[data-emojiable=true]',
-        assetsPath: 'lib/img/',
-        popupButtonClasses: 'uil uil-smile-squint-wink-alt'
+function checkboxStyle() {
+    // wrap the input in a new div that we can style to hide the default checkboxes
+    $(".check").wrap("<div class='new'></div>");
+
+    // add a span after each checkbox that we can style as our new checkboxes
+    $('input.custom__checkbox[type="checkbox"]').after('<span class="check-box"></span>');
+
+    $('input.custom__checkbox[type="radio"]').after('<span class="check-box"></span>');
+}
+
+checkboxStyle();
+
+//slider
+function slider__lot__page() {
+    $('.slider__header').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider__nav'
     });
-    // Finds all elements with `emojiable_selector` and converts them to rich emoji input fields
-    // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
-    // It can be called as many times as necessary; previously converted input fields will not be converted again
-    window.emojiPicker.discover();
-});
+    $('.slider__nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider__header',
+        dots: true,
+        arrows: true,
+        centerMode: true,
+        focusOnSelect: true
+    });
+}
 
-//menu mobile
-$('.mobile__menu').on('click', function () {
-    $('.mibile__menu__wrap').addClass('open');
-    if ($(window).width() < 589) {
-        $('body').addClass('no__scroll');
-    }
+$('.recommended__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider__header',
+    dots: false,
+    arrows: true,
+    centerMode: false,
+    focusOnSelect: false
 });
-$('.mibile__menu__wrap').find('.btn__close').on('click', function () {
-    $('.mibile__menu__wrap').removeClass('open');
-    $('body').removeClass('no__scroll');
-});
+slider__lot__page();
 
-//--------------------------------------------
-$('.vocabulary__mini').on('click', function () {
-    if (event.target != this) {
-        return;
-    }
-    $('.vocabulary__mini').toggleClass('open');
-    $(this).find('.close__modal').on('click', function () {
-        $('.vocabulary__mini').removeClass('open');
+$('.info__wrap').find('i').on('click', function (e) {
+    $('.info__desc').toggleClass('active');
+    $(document).mouseup(function (e) {
+        var container = $('.info__wrap').find('i');
+        if (container.has(e.target).length === 0) {
+            $('.info__desc').removeClass('active');
+        }
     });
 });
-$('.request-call,.btn__request-call').on('click', function () {
-    $('.modal__call').parents('.modal__wrap').addClass('active');
-    $('body').addClass('no__scroll');
 
-    $('.modal__call').find('.close__modal').on('click', function () {
-        $(this).parents('.modal__wrap').removeClass('active');
-        $('body').removeClass('no__scroll');
-    });
-});
-$('.notification__nav__tabs').find('.tabs').on('click', function () {
-    $('.notification__nav__tabs').find('.tabs').removeClass('active');
-    $(this).addClass('active');
-});
-
-$('.request-call__mobile__wrap__button').on('click', function () {
-    $('.request-call__mobile__wrap').toggleClass('open');
-    $(this).text('Закрыть');
-
-    if (!$('.request-call__mobile__wrap').hasClass('open')) {
-        $(this).text('Заказать звонок');
-    }
-});
+var myElement = document.getElementById('simple-bar__table');
+new SimpleBar(myElement, { autoHide: true });
 
 /***/ })
-
-/******/ });
+/******/ ]);
 //# sourceMappingURL=main.js.map
