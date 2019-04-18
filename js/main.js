5d81323a4e6a06e33c7dcec1874ca7070b4d0a1a
nickyshibaev@gmail.com <GrAnT0132>
2019-04-18 12:23:25 +0300

    faq and my__lot page

--- js/main.js
+++ js/main.js
@@ -444,6 +444,16 @@ function notification__bell() {
 }
 notification__bell();
 
+$(document).mouseup(function (e) {
+    // событие клика по веб-документу
+    var div = $(".notification__wrap"); // тут указываем ID элемента
+    if (!div.is(e.target) // если клик был не по нашему блоку
+    && div.has(e.target).length === 0) {
+        // и не по его дочерним элементам
+        $('.notification__wrap').removeClass('open'); // скрываем его
+    }
+});
+
 /***/ })
 /******/ ]);
 //# sourceMappingURL=main.js.map
